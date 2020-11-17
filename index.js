7726708f0a7309a13cd7128f6c4e751c56b67260
aybel <j.abel.vera@gmail.com>
2020-11-17 11:49:25 -0600

    cambio en puerto

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //configurar el servidor de express
-const express=require('express');
-const conectarDB=require('./config/db');
-const cors=require('cors');
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
 //crear el servidor
-const app =express();
+const app = express();
 
 
 //conectar a la base datos
@@ -11,18 +11,18 @@ conectarDB();
 //habilitar cors
 app.use(cors());
 //habilitar express.json para leer datos
-app.use(express.json({extended:true}));
+app.use(express.json({ extended: true }));
 
 //puerto de la app
-const port=process.env.port || 4000;
+const port = process.env.port || 4000;
 
 //importar rutas
-app.use('/api/usuarios',require('./routes/usuarios'));
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/proyectos',require('./routes/proyectos'));
-app.use('/api/tareas',require('./routes/tareas'));
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/proyectos', require('./routes/proyectos'));
+app.use('/api/tareas', require('./routes/tareas'));
 //arrancamos el servidor
 
-app.listen(port,'0.0.0.0',()=>{
-    console.log(`El servidor esta corriendo en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta corriendo en el puerto ${port}`);
 })
