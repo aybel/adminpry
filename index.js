8153021c63cb64db0fa6ca86245836ece5752da7
aybel <j.abel.vera@gmail.com>
2020-11-17 11:28:39 -0600

    cambio de puerto

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({extended:true}));
 
 //puerto de la app
-const PORT=process.env.PORT || 4000;
+const port=process.env.port || 4000;
 
 //importar rutas
 app.use('/api/usuarios',require('./routes/usuarios'));
@@ -23,6 +23,6 @@ app.use('/api/proyectos',require('./routes/proyectos'));
 app.use('/api/tareas',require('./routes/tareas'));
 //arrancamos el servidor
 
-app.listen(PORT,()=>{
+app.listen(port,'0.0.0.0',()=>{
     console.log(`El servidor esta corriendo en el puerto ${PORT}`);
 })
